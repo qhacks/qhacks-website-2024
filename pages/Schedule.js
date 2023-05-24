10bd7a8dd8a3a79aea5b7d06e0a8983be451be76
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-05-24 11:39:59 -0500

    Created file structure with pages folder

---
+++ pages/Schedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Schedule = () => {
+  return (
+    <div>
+      <h1 className='bg-gray-950' >Scheduile Page</h1>
+      <p className='' >I love money.</p>
+    </div>
+  );
+};
+
+export default Schedule;
