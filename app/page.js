f8a70b8b3e608caee672a86fe3d1e0746d1c9574
Jacob Tepperman <jtepp@icloud.com>
2023-05-26 22:14:05 -0400

    moved header & footer from page.js to layout.js (so it's applied to all pages)

--- app/page.js
+++ app/page.js
@@ -48,18 +48,9 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-left  bg-gray-950 ">
+    <main className="">
       <Image className="absolute top-0 right-20" src="/MLH.svg" alt="Major Hacking Leaguge Logo" width={127} height={222}/>
-      
-      <nav className="flex flex-row justify-between">
-        <div className="flex flex-row items-left justify-left">
-          <a href='#' className="text-2xl p-4 font-bold text-white">Home</a>
-          <a href='#about' className="text-2xl p-4 font-bold text-white">About</a>
-          <a href='#' className="text-2xl p-4 font-bold text-white">Sponsors</a>
-          <a href='#' className="text-2xl p-4 font-bold text-white">Contact</a>
-          <a href='/Schedule' className="text-2xl p-4 font-bold text-white">Schedule</a>
-        </div>
-      </nav>
+
 
       <section className='flex items-center justify-center'>
         <div className=' flex flex-row justify-between'>
@@ -168,31 +159,6 @@ export default function Home() {
           <SponsorCard sponsor={voiceflow} />
         </div>
       </section>
-
-      <footer className='pt-48'>
-        <div className='flex justify-center items-center py-5'>
-          <a className='px-[3%] z-2 ' href='https://www.instagram.com/qhacks24/'><span className='text-5xl'><i class="fab fa-instagram text-white"></i></span></a>
-          <a className='px-[3%] z-2 ' href='https://twitter.com/QHacks23'><span className='text-5xl'><i class="fab fa-twitter text-white"></i></span></a>
-          <a className='px-[3%] z-2 ' href='mailto:hello@qhacks.io'><span className='text-5xl'><i class="far fa-envelope text-white"></i></span></a>
-          <a className='px-[3%] z-2 ' href='https://www.facebook.com/QHacks/'><span className='text-5xl'><i class="fab fa-facebook text-white"></i></span></a>
-          <a className='px-[3%] z-2 ' href='https://www.linkedin.com/company/qhacks/'><span className='text-5xl'><i class="fab fa-linkedin text-white"></i></span></a>
-        </div>
-        <div className='flex justify-center items-center py-2'>
-          <a href='https://qhacks.io/' className='text-white hover:text-blue-400 px-[3%]'>QHacks 2023</a>
-          <a href='https://ghw.mlh.io/' className='text-white hover:text-blue-400 px-[3%]'>Local Hack Day</a>
-          <a href='https://medium.com/@qhacks' className='text-white hover:text-blue-400 px-[3%]'>QHacks Blog</a>
-          <a href='https://www.youtube.com/watch?v=a3Z7zEc7AXQ' className='text-white hover:text-blue-400 px-[3%]'>Hack Day Tips</a>
-          <a href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf' className='text-white hover:text-blue-400 px-[3%]'>MLH Code of Conduct</a>
-        </div>
-        <div>
-
-        </div>
-        <div className='flex justify-center items-center pb-2'>
-          <p className='text-white px-[2%]'>Queen's University</p>
-          <p className='text-white px-[2%]'>99 University Ave, Kingston ON</p>
-          <p className='text-white px-[2%]'>Copyright © 2023 QHACKS</p>
-        </div>
-      </footer>
     </main>
   )
 }
