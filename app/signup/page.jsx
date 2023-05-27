f64f49afe5cc3add2bbc9ef13c7cb49da6f8cdf3
Jacob Tepperman <jtepp@icloud.com>
2023-05-27 13:43:57 -0400

    made whole signup card text-white

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 export default function SignUp() {
     return ( 
         <div className='h-[800px]'>
-            <form id='signup-container' className='flex flex-col justify-center items-center
-             bg-waves w-full h-1/4 bg-repeat-x bg-bottom h-full' style={{
+            <form id='signup-container' className='flex flex-col justify-center items-center w-full h-1/4 bg-repeat-x bg-bottom h-full' style={{
                 backgroundImage: `url(${wavesSrc.src})`,
                 backgroundSize: '1200px 300px'
             }}>
@@ -17,8 +16,8 @@ export default function SignUp() {
                 <div className='overflow-hidden whitespace-nowrap'>QHACKS QHACKS QHACKS QHACKS QHACKS QHACKS QHACKS</div>
             </div>
 
-            <div id="signup-card" className='z-[1] p-[50px] bg-zinc-900 rounded-[15px] flex flex-col justify-center items-center gap-[30px]'>
-                <h2 className='text-3xl font-bold text-white'>Welcome</h2>
+            <div id="signup-card" className='text-white z-[1] p-[50px] bg-zinc-900 rounded-[15px] flex flex-col justify-center items-center gap-[30px]'>
+                <h2 className='text-3xl font-bold'>Welcome</h2>
                 <input type='email' className='w-[280px] border-b border-solid border-[#717171] focus:outline-none' style={{
                     background: 'none',
                 }} placeholder='Email'></input>
@@ -28,7 +27,7 @@ export default function SignUp() {
                 <input type='password' className='w-[280px] border-b border-solid border-[#717171] focus:outline-none' style={{
                     background: 'none',
                 }} placeholder='Confirm Password'></input>
-                <input type="submit" value="Create Account" className='font-semibold w-[280px] text-xl text-white px-5 py-3 rounded-xl' style={{
+                <input type="submit" value="Create Account" className='font-semibold w-[280px] text-xl px-5 py-3 rounded-xl' style={{
                     background: "linear-gradient(90deg, rgba(255, 0, 0, 0.72) -6.16%, rgba(255, 138, 0, 0.83) 60.41%)"
                 }}/>
                 
