5b70df5fcb38aa4d1426c5e2968def973bbaaa9c
Jacob Tepperman <jtepp@icloud.com>
2023-08-27 20:42:50 -0400

    added crown loading gif

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import loader from '../public/loader.gif'
 
 function LoadingScreen(props) {
     return ( 
@@ -6,7 +7,8 @@ function LoadingScreen(props) {
             <div id='loading-animation-container' className='text-white text-center'>
                 <div>
                     <div class="flex justify-center items-center h-screen">
-                        <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+                        {/* <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div> */}
+                        <img src={loader.src} className='max-w-[250px]' alt="" srcset="" />
                     </div>
                 </div>
                 
