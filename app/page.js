87c0e6b317781cd9b0ab1c05768c17e63f89f946
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-09-08 14:18:28 -0400

    primitive wave design animation finsihed

--- app/page.js
+++ app/page.js
@@ -34,21 +34,96 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	const [showLoadingScreen, setShowLoadingScreen] = useState(true);
-	// useEffect(() => {
-	// 	setShowLoadingScreen(false);
-	// }, [setShowLoadingScreen]);
+
+	const [windowWidth, setWindowWidth] = useState(0);
+	useEffect(() => {
+		// Function to update window width
+		const updateWindowWidth = () => {
+		setWindowWidth(window.innerWidth);
+		};
+
+		// Initial update
+		updateWindowWidth();
+
+		// Add a resize event listener to update width when the window is resized
+		window.addEventListener("resize", updateWindowWidth);
+
+		// Clean up the event listener when the component unmounts
+		return () => {
+		window.removeEventListener("resize", updateWindowWidth);
+		};
+	}, []);
+
+	console.log(windowWidth);
+
 
 	return (
 		<main className="bg-gray-950 w-screen">
-			<motion.div
-				className="w-full overflow-hidden border h-48"
-				initial={{ x: '0%' }}
-				animate={{ x: '100%' }}
-				transition={{ duration: 20, repeat: Infinity, ease: 'linear' }}
-			>
-				<img className="w-[5000px] absolute top-0 -z-1" src="/warmWaves.svg" alt="a graphic of some warm colred waves" width={4000}/>
-			</motion.div>
-		
+			<a href="#" className="fixed bottom-4 left-4">
+				Back to top
+			</a>
+			<div className="absolute top-0 w-full h-64 overflow-hidden">
+				{/* <motion.div
+					className="w-full overflow-hidden border h-48"
+					initial={{ x: '0%' }}
+					animate={{ x: '100%' }}
+					transition={{ duration: 20, repeat: Infinity, ease: 'linear' }}
+				>
+					<img className="w-[5000px] absolute top-0 -z-1" src="/warmWaves.svg" alt="a graphic of some warm colred waves" width={4000}/>
+				</motion.div> */}
+				
+				<motion.div
+					className="absolute w-[2200px] h-full overflow-hidden top-0 "
+					initial={{ x: "0%" }} 
+					animate={{ x: "-18%" }} 
+					exit={{ x: "18%" }} 
+					transition={{ duration: 14, repeat: Infinity, ease: "easeOut", repeatType: "reverse"  }} // Animation duration and easing
+
+				>
+					<img className="w-full absolute top-0 rotate-180 transform" src="/wave1.svg" alt=""></img>
+				</motion.div>
+				<motion.div
+					className="absolute w-[2200px] h-full overflow-hidden top-0"
+					initial={{ x: "0%" }} 
+					animate={{ x: "-18%" }} 
+					exit={{ x: "18%" }} 
+					transition={{ duration: 16, repeat: Infinity, ease: "easeOut", repeatType: "reverse"  }} // Animation duration and easing
+
+				>
+					<img className="w-full absolute top-0 rotate-180 transform" src="/wave2.svg" alt=""></img>
+				</motion.div>
+				<motion.div
+					className="absolute w-[2200px] h-full overflow-hidden top-0"
+					initial={{ x: "0%" }} 
+					animate={{ x: "18%" }} 
+					exit={{ x: "-18%" }} 
+					transition={{ duration: 13, repeat: Infinity, ease: "easeOut", repeatType: "reverse"  }} // Animation duration and easing
+
+				>
+					<img className="w-full absolute top-0 rotate-180 transform" src="/wave3.svg" alt=""></img>
+				</motion.div>
+				<motion.div
+					className="absolute w-[2200px] h-full overflow-hidden top-0"
+					initial={{ x: "0%" }} 
+					animate={{ x: "-18%" }} 
+					exit={{ x: "18%" }} 
+					transition={{ duration: 17, repeat: Infinity, ease: "easeOut", repeatType: "reverse"  }} // Animation duration and easing
+
+				>
+					<img className="w-full absolute top-0 rotate-180 transform" src="/wave4.svg" alt=""></img>
+				</motion.div>
+				<motion.div
+					className="absolute w-[2200px] h-full overflow-hidden top-0"
+					initial={{ x: "0%" }} 
+					animate={{ x: "18%" }} 
+					exit={{ x: "-18%" }} 
+					transition={{ duration: 18, repeat: Infinity, ease: "easeOut", repeatType: "reverse"  }} // Animation duration and easing
+
+				>
+					<img className="w-full absolute top-0 rotate-180 transform" src="/wave5.svg" alt=""></img>
+				</motion.div>
+			</div>
+
 			<Navbar />
 			{/* Loader */}
 			<LoadingScreen
