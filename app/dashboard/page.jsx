bcdc52658e1974999bcf4f6656d59c16165653a2
Jacob Tepperman <jtepp@icloud.com>
2023-09-28 14:51:46 -0400

    added application link buttons

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import '../../css/background.css';
-
+import ApplicationLinkButton from '../../components/dashboard/ApplicationLinkButton';
 
 export default function Dashboard() {
     return(
@@ -13,10 +13,15 @@ export default function Dashboard() {
                     <div className='bg-[#202020] w-full h-[600px] flex flex-col justify-center items-center rounded-2xl mx-4 my-2'>
                         <h3 className='text-white font-bold text-2xl mb-8'>Application Tracker</h3>
                         <div className='w-full flex flex-col'>
-                            <div className='w-[90%] h-16 bg-[#474747] rounded-[15px] my-2 ml-4'>Test1</div>
-                            <div className='w-[90%] h-16 bg-[#474747] rounded-[15px] my-2 ml-4'>Test1</div>
-                            <div className='w-[90%] h-16 bg-[#474747] rounded-[15px] my-2 ml-4'>Test1</div>
-                            <div className='w-[90%] h-16 bg-[#474747] rounded-[15px] my-2 ml-4'>Test1</div>
+                            {/* Statuses
+                            - 0: grey Go (incomplete but not available)
+                            - 1: yellow Go (incomplete and available)
+                            - 2: green Edit (complete and available)
+                            */}
+                            <ApplicationLinkButton title="About" status={2} dest={null}/>
+                            <ApplicationLinkButton title="Education" status={2} dest={null}/>
+                            <ApplicationLinkButton title="Additional Info" status={1} dest={null}/>
+                            <ApplicationLinkButton title="Policies" status={0} dest={null}/>
                         </div>
                     </div>
                     <div className='bg-[#202020] w-full h-96 flex flex-col justify-center items-center rounded-2xl mx-4 my-2'>
