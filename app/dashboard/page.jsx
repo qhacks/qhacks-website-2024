2834b862a8c6372851b7ac9b9e9b59926bceb02c
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-10-02 17:55:22 -0400

    Mobile view for dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,55 +5,59 @@ import ApplicationStatusTracker from '../../components/dashboard/ApplicationStat
 
 export default function Dashboard() {
     return(
-        <div className='h-screen w-full bg-[#111010] bg-topogrophy-dark flex flex-col'>
-            <div className="h-full w-full flex flex-row justify-center items-start pt-[3rem]">
-                <div className='h-[90%] w-[23%] flex justify-center items-center m-4 flex-col'>
-                    <div className='w-full h-50px flex'>
-                        <a 
-                            className='bg-[#FE0000] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold mb-[10px]'
-                            href='/'
-                        >Home</a>
-                    </div>
-                    <div className='bg-[#202020] w-full min-w-fit h-[600px] flex flex-col justify-center items-center rounded-2xl mx-4 my-2 py-3'>
-                        <h3 className='text-white font-bold text-2xl mb-1 text-center'>Application Tracker</h3>
-                        <div className='w-full flex flex-col'>
-                            {/* Statuses
-                            - 0: grey Go (incomplete but not available)
-                            - 1: yellow Go (incomplete and available)
-                            - 2: green Edit (complete and available)
-                            */}
-                            <ApplicationLinkButton title="About" status={2} dest={"/registration/about"}/>
-                            <ApplicationLinkButton title="Education" status={2} dest={"/registration/education"}/>
-                            <ApplicationLinkButton title="Additional Info" status={1} dest={"/registration/application"}/>
-                            <ApplicationLinkButton title="Policies" status={0} dest={"/registration/policies"}/>
+        <div className='h-fit md:h-screen w-full bg-[#111010] bg-topogrophy-dark flex flex-col'>
+            {/* Home Button */}
+            <div className='w-full flex justify-center items-center mt-[3rem]'>
+                <div className='w-[90%] md:w-[75%] h-50px flex'>
+                    <a href='/' className='bg-[#FE0000] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold mb-[10px]'>Home</a>
+                </div>
+            </div>
+            
+
+            <div className="h-full w-full flex md:flex-row flex-col-reverse justify-center items-center md:items-start">
+                <div className='h-[90%] w-[90%] md:w-[30%] xl:w-[20%] flex justify-center items-center flex-col'>
+                    <div className='flex flex-col sm:flex-row md:flex-col w-full h-full justify-between'>
+                        {/* Application Tracker */}
+                        <div className='m-2 bg-[#202020] w-auto sm:w-2/3 md:w-full min-w-fit h-[600px] md:h-3/5 flex flex-col justify-center items-center rounded-2xl'>
+                            <h3 className='text-white font-bold text-2xl mb-2 text-center mx-7'>Application Tracker</h3>
+                            <div className='w-full flex flex-col'>
+                                {/* Statuses
+                                - 0: grey Go (incomplete but not available)
+                                - 1: yellow Go (incomplete and available)
+                                - 2: green Edit (complete and available)
+                                */}
+                                <ApplicationLinkButton title="About" status={2} dest={"/registration/about"}/>
+                                <ApplicationLinkButton title="Education" status={2} dest={"/registration/education"}/>
+                                <ApplicationLinkButton title="Additional Info" status={1} dest={"/registration/application"}/>
+                                <ApplicationLinkButton title="Policies" status={0} dest={"/registration/policies"}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className='bg-[#202020] w-full h-96 flex flex-col justify-center items-center rounded-2xl mx-4 my-2'>
-                        <h3 className='text-white font-bold text-2xl mb-8'>Need a team?</h3>
-                        <a 
-                            className='bg-[#FAAF40] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold'
-                            href='/'
-                        >More Info</a>
-                    </div>
-                    <div className='bg-[#202020] w-full h-96 flex flex-col justify-center items-center rounded-2xl mx-4 my-2'>
-                        <h3 className='text-white font-bold text-2xl mb-8'>Need a bus?</h3>
-                        <a 
-                            className='bg-[#FAAF40] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold'
-                            href='/'    
-                        >More Info</a>
+                        {/* Info Buttons */}
+                        <div className='w-full sm:w-1/3 md:w-auto h-fit sm:h-[600px] md:h-2/5 flex flex-col justify-between'>
+                            <div className='m-2 bg-[#202020] w-auto h-44 sm:h-[48%] md:h-1/2 flex flex-col justify-center items-center rounded-2xl'>
+                                <h3 className='text-white font-bold text-2xl mb-4'>Need a team?</h3>
+                                <a href='/' className='bg-[#FAAF40] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold'>More Info</a>
+                            </div>
+                            <div className='m-2 bg-[#202020] w-auto h-44 sm:h-[48%] md:h-1/2 flex flex-col justify-center items-center rounded-2xl'>
+                                <h3 className='text-white font-bold text-2xl mb-4'>Need a bus?</h3>
+                                <a href='/' className='bg-[#FAAF40] w-[160px] h-[50px] rounded-full flex justify-center items-center text-xl text-white font-bold'>More Info</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 
-                <div className='h-[calc(90%-50px-1rem)] w-[55%] flex flex-col justify-start items-center bg-[#202020] mt-[calc(60px+1.5rem)] rounded-2xl'>
-                    <ApplicationStatusTracker status={2} />
-                    <div className='w-full h-[500px] rounded-2xl text-white py-[3rem] px-[2rem]'>
-                        <div className='text-2xl mb-2 font-bold'>QHacks Application</div>
+                <div className='h-[90%] w-[90%] md:w-[55%] xl:w-[55%] flex flex-col justify-start items-center m-3'>
+                    <div className='m-2 bg-[#202020] rounded-2xl h-full'>
+                        <ApplicationStatusTracker status={0} />
+                        <div className='w-full h-[500px] rounded-2xl text-white py-[3rem] px-[2rem]'>
+                            <div className='text-2xl mb-2 font-bold'>QHacks Application</div>
 
-                        <div>
-                        QHacks is Queen's University's largest Hackathon! Join over 500 hackers, speakers and mentors to create, learn and share your ideas. Attend our workshops and hacker challenges and meet industry professionals, Innovators, and creatives from all faculties and skill levels are welcomed.
-                        <br />
-                        <br />
-                        Are you from out of town? No worries! QHacks offers reimbursement for Mega bus tickets.
+                            <div>
+                            QHacks is Queen's University's largest Hackathon! Join over 500 hackers, speakers and mentors to create, learn and share your ideas. Attend our workshops and hacker challenges and meet industry professionals, Innovators, and creatives from all faculties and skill levels are welcomed.
+                            <br />
+                            <br />
+                            Are you from out of town? No worries! QHacks offers reimbursement for Mega bus tickets.
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/dashboard/ApplicationStatusTracker.jsx
+++ components/dashboard/ApplicationStatusTracker.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 function ApplicationStatusTracker(props) {
     return ( 
         <div className='bg-[#2D2D2D] w-[calc(100%-2rem)] flex flex-col justify-start items-center m-[1rem] rounded-xl'>
-            <div className='text-white text-4xl font-semibold py-[1.5rem]'>
+            <div className='text-white text-center text-4xl font-semibold py-[1.5rem]'>
                 Application Status
             </div>
-            <div className='w-[calc(100%-3rem)] h-[62px] mb-[20px] flex justify-center'>
+            <div className='hidden sm:flex w-[calc(100%-3rem)] h-[62px] mb-[20px]  justify-center'>
                 <img src={`/application-status/${props.status}.svg`} alt="" srcset="" />
+            </div>
+            <div className='flex sm:hidden py-10 w-[calc(100%-3rem)] h-[500px] justify-center'>
+                <img className=' w-full' src={`/application-status-vertical/${props.status}.svg`} alt="" srcset="" />
             </div>
         </div>
      );
