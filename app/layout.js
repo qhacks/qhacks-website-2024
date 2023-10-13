5f0607e72c67ac2241848efe64a1818cad3d96cb
spy772 <edward.tanurkov@gmail.com>
2023-10-13 13:13:35 -0400

    initial metadata API implementation for home page
    - Removes "use client" since metadata couldn't be exported with it present
    - Removes the other two imports since they cause compilation errors without "use client" being present
    - New metadata object is added and displays "QHacks 2024" in the bar on the browser
    - Anyone can add metadata to another page and update the title for that page (e.x., in our Sponsors page do title: {"Sponsors"} and it should show up as "Sponsors | QHacks 2024" if I implemented this correctly)
    - Description should show up during searches as shown
    - Keywords should help get our website higher in the search results
    - Icons should display the favicon, but for some reason it doesn't yet show up on my device; might need to fix that later

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,19 @@
-"use client";
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { useState } from 'react';
-import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+	title: {
+    template: '%s | QHacks 2024',
+    default: 'QHacks 2024'
+  },
+	description: 'Will QHACKS 2024 be in-person or virtual? What is a hackathon? Who can attend QHacks? How much does it cost to attend QHacks?',
+  keywords: ['QHacks', 'QHacks 2024', 'Queens Hackathon'],
+  icons: {  
+    icon: '/favicon.ico',
+  },
+}
 
 export default function RootLayout({ children }) {  
   return (
