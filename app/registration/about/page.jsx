c505e9929b75f49f83f45b556e97fef352cc636f
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2023-10-17 17:33:38 -0400

    Update page.jsx

--- app/registration/about/page.jsx
+++ app/registration/about/page.jsx
@@ -283,6 +283,11 @@ export default function About() {
             />
           </div>
 
+          {/* Resume */}
+          <div className="">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="large_size">Large file input</label>
+            <input class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="large_size" type="file"/>
+          </div>
           {/* create input for github Link */}
           <div className="flex flex-col mb-[2rem]">
             <label htmlFor="github">Github Link</label>
