1ca5483ccc5adff53a15bb3e9e4f20f0a7f04f62
Joash Mathew <joashm14@gmail.com>
2023-10-18 14:45:16 -0400

    chore: add email verification on account creation

--- firebase/auth/signup.js
+++ firebase/auth/signup.js
@@ -1,6 +1,7 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
 import { getFirebase } from '../firebase';
 import createUser from "../firestore/createUser";
+import logout from "./logout";
 
 const { firebase } = getFirebase();
 const auth = getAuth(firebase);
@@ -13,7 +14,13 @@ export default async function signUp(email, password)
     try
     {
         result = await createUserWithEmailAndPassword(auth, email, password).then(async (res) => {
-            await createUser(res.user.uid, email).then(() => {
+            await createUser(res.user.uid, email).then(async () => {
+                await sendEmailVerification(auth.currentUser).then(async () => {
+                    await logout();
+                }).catch((e) => {
+                    console.error('Error sending email verification: ', e);
+                    error = e;
+                });
             }).catch((e) => {
                 console.error('Error writing document: ', e);
                 error = e;
