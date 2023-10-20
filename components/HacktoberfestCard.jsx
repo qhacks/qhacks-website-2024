2f67bc5c9d7734dccd075aaf0c09227ea7b7f349
Eric <85941655+EMedina456@users.noreply.github.com>
2023-10-20 13:55:20 -0400

    Added Hacktoberfest ad and made links external

--- components/HacktoberfestCard.jsx
+++ components/HacktoberfestCard.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-export default function FirstYearCard() {
+export default function HacktoberfestCard() {
 
     return(
         <div className="m-4 border-2 rounded-2xl w-72 md:w-96 2xl:w-[600px] h-44 2xl:h-60 p-2 md:p-4 flex flex-row items-center justify-center">
@@ -17,13 +17,14 @@ export default function FirstYearCard() {
             <div  className="2xl:px-3 flex justify-center">
                 <div className="text-center p-4">
                     <h4 className="text-white 2xl:text-2xl font-bold">
-                        We're currently hiring First Year Representatives! Applications due September 22nd, 2023.
+                        RSVP to our Hacktoberfest Event!
                     </h4>
                     <a
-                        href="https://docs.google.com/forms/d/e/1FAIpQLSeFeu7YHRbxE5tzoKgG6KtCCpqJMrji00e430VOLTaFVKUWVg/viewform"
+                        href="https://forms.gle/YR4pCfrefvmvqonP9"
                         className="text-white hover:text-sky-500"
+                        target="_blank"
                     >
-                        <motion.p whileHover={{scale:1.1}} className="2xl:text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">Application Link</motion.p>
+                        <motion.p whileHover={{scale:1.1}} className="2xl:text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">RSVP Here</motion.p>
                     </a>
                 </div>
             </div>
