b762385bd00d98639d7b383048f25e80a303421f
Eric <85941655+EMedina456@users.noreply.github.com>
2023-10-20 22:59:09 -0400

    fixes for about (resume needs firebase access)

--- firebase/storage/checkIfFileExists.js
+++ firebase/storage/checkIfFileExists.js
@@ -2,7 +2,7 @@ import { ref, getDownloadURL } from 'firebase/storage';
 import { getFirebase } from '../firebase';
 const { storage } = getFirebase();
 
-export default function checkIfFileExists(filePath){
+export default function checkIfFileExists(uid) {
   const storageRef = ref(storage, `resumes/${uid}/resume.pdf`);
 
   getDownloadURL(storageRef)
