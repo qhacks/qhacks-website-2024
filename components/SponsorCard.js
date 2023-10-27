935aeb35c5ddcc74ccc83a641e99ae9290cd8bc8
spy772 <edward.tanurkov@gmail.com>
2023-10-26 23:05:36 -0400

    updated sponsor image sizing and removed "10 workshops" bubble
    - Implemented the different image sizes for our sponsors
    - Kilo-Tera tier in current sponsors (Tera tier is largest), previous sponsors is smallest
    - Removed the "10 Workshops" bubble found next to the "11 Schools", "500+ Hackers" bubbles too

--- components/SponsorCard.js
+++ components/SponsorCard.js
@@ -13,30 +13,33 @@ const SponsorCard = ({ sponsor }) => {
     let color3 = 'border-yellow-600';
   }
 
+  let height;
+  let width;
+
   // width and height variables
   if (tier == 'tera') {
-    let width = 'w-32 xs:w-[500px]';
-    let height = 'h-20 xs:h-64';
+    width = 'w-32 xs:w-[500px]';
+    height = 'h-20 xs:h-64';
   } else if (tier == 'giga') {
-    let width = 'w-32 xs:w-[400px]';
-    let height = 'h-20 xs:h-56';
+    width = 'w-32 xs:w-[400px]';
+    height = 'h-20 xs:h-56';
   } else if (tier == 'mega') {
-    let width = 'w-32 xs:w-[300px]';
-    let height = 'h-20 xs:h-44';
+    width = 'w-32 xs:w-[300px]';
+    height = 'h-20 xs:h-44';
   } else if (tier == 'kilo') {
-    let width = 'w-32 xs:w-[200px]';
-    let height = 'h-20 xs:h-32';
+    width = 'w-32 xs:w-[200px]';
+    height = 'h-20 xs:h-32';
   }
   else if (tier == 'previous') {
-    let width = 'w-32 xs:w-[150px]';
-    let height = 'h-20 xs:h-24';
+    width = 'w-32 xs:w-[150px]';
+    height = 'h-20 xs:h-24';
   }
 
   return (
     <motion.a 
       href={link} 
       // Remove the width and height props and replace them with the variables I created above
-      className={`justify-center flex h-20 xs:h-44 w-32 xs:w-[264px] ${borderColor} xs:m-2 m-1 border-4 rounded-xl bg-white`}
+      className={`justify-center flex ${height} ${width} ${borderColor} xs:m-2 m-1 border-4 rounded-xl bg-white`}
       whileHover={{ scale: 1.1 }}
     >
       <img src={logo} alt={`${name} logo`} className="rounded-xl"/>
